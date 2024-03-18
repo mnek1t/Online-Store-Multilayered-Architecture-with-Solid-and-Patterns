be9e61b69eae213f71860a2ca2a9320c2ed8aa8a
mnek1t <mednikita2004@gmail.com>
2024-03-18 22:49:31 +0200

    DAL for Admin

--- StoreDAL/Repository/CategoryRepository.cs
+++ StoreDAL/Repository/CategoryRepository.cs
@@ -1,4 +1,6 @@
-﻿using StoreDAL.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using StoreDAL.Data;
+using StoreDAL.Entities;
 using StoreDAL.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -8,41 +10,65 @@ using System.Threading.Tasks;
 
 namespace StoreDAL.Repository
 {
-    public class CategoryRepository : ICategoryRepository
+    public class CategoryRepository : AbstractRepository, ICategoryRepository
     {
+        private readonly DbSet<Category> dbSet;
+        public CategoryRepository(StoreDbContext context) : base(context) 
+        {
+            dbSet = context.Set<Category>();
+        }
         public void Add(Category entity)
         {
-            throw new NotImplementedException();
+            dbSet.Add(entity);
+            context.SaveChanges();
         }
 
         public void Delete(Category entity)
         {
-            throw new NotImplementedException();
+            dbSet.Remove(entity);
+            context.SaveChanges();
         }
 
         public void DeleteById(int id)
         {
-            throw new NotImplementedException();
+            var entity = dbSet.Find(id);
+            if (entity != null)
+            {
+                dbSet.Remove(entity);
+                context.SaveChanges();
+            }
         }
 
         public IEnumerable<Category> GetAll()
         {
-            throw new NotImplementedException();
+            return dbSet.ToList();
         }
 
         public IEnumerable<Category> GetAll(int pageNumber, int rowCount)
         {
-            throw new NotImplementedException();
+            return dbSet.Take(rowCount).ToList();
         }
 
         public Category GetById(int id)
         {
-            throw new NotImplementedException();
+            return dbSet.Find(id);
         }
 
         public void Update(Category entity)
         {
-            throw new NotImplementedException();
+            var category = dbSet.Find(entity.Id);
+            if (category == null)
+                Add(category);
+            else
+            {
+                dbSet.Remove(category);
+                context.SaveChanges();
+                category.Id = entity.Id;
+                category.Name = entity.Name;
+                category.Titles = entity.Titles;
+                dbSet.Add(category);
+                context.SaveChanges();
+            }
         }
     }
 }
