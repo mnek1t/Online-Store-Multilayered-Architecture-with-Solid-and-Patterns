a304ec51848fb9742fa427429209f46354a012d1
mnek1t <mednikita2004@gmail.com>
2024-03-18 22:42:19 +0200

    Presentation layer for Registered User Functionality

--- StoreBLL/Services/OrderDetailService.cs
+++ StoreBLL/Services/OrderDetailService.cs
@@ -3,6 +3,7 @@ using StoreBLL.Models;
 using StoreDAL.Data;
 using StoreDAL.Entities;
 using StoreDAL.Interfaces;
+using StoreDAL.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,29 +14,36 @@ namespace StoreBLL.Services
 {
     public class OrderDetailService : ICrud
     {
+        IOrderDetailRepository repository;
         public OrderDetailService(StoreDbContext context)
         {
+            repository = new OrderDetailRepository(context);
         }
 
         public void Add(AbstractModel model)
         {
-            throw new NotImplementedException();
+            var x = (OrderDetailModel)model;
+            repository.Add(new OrderDetail(x.Id, x.OrderId,x.ProductId,x.Price, x.ProductAmount));
         }
         public void Delete(int modelId)
         {
-            throw new NotImplementedException();
+            repository.DeleteById(modelId);
         }
         public IEnumerable<AbstractModel> GetAll()
         {
-            throw new NotImplementedException();
+            return repository
+                .GetAll()
+                .Select(x => new OrderDetailModel(x.Id, x.OrderId, x.ProductId, x.Price, x.ProductAmount));
         }
         public AbstractModel GetById(int id)
         {
-            throw new NotImplementedException();
+            var res = repository.GetById(id);
+            return new OrderDetailModel(res.Id, res.OrderId, res.ProductId, res.Price, res.ProductAmount);
         }
         public void Update(AbstractModel model)
         {
-            throw new NotImplementedException();
+            var x = (OrderDetailModel)model;
+            repository.Update(new OrderDetail(x.Id, x.OrderId, x.ProductId,x.Price,x.ProductAmount));
         }
     }
 }
